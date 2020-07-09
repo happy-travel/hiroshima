2d8131e420c5843015f9dca15cfad8e97ce05151
Oleg Konovalov <oleg.konovalov@happytravel.com>
2020-07-09 03:51:24 +0300

    Remove an extra line

--- HappyTravel.Hiroshima.Data/Models/User.cs
+++ HappyTravel.Hiroshima.Data/Models/User.cs
@@ -10,7 +10,6 @@ namespace HappyTravel.Hiroshima.Data.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Position { get; set; }
-        
         public DateTime Created { get; set; }
     }
 }
