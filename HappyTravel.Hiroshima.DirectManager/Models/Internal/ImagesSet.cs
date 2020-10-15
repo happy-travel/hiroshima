d5a8de57152830482ad406ff0cf7389fc33bf183
Alexey Shokarev <alexey.shokarev@opentech.kz>
2020-10-15 11:32:43 +0600

    Added validations for image

---
+++ HappyTravel.Hiroshima.DirectManager/Models/Internal/ImagesSet.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HappyTravel.Hiroshima.DirectManager.Models.Internal
+{
+    public class ImagesSet
+    {
+        public string Name { get; set; }
+        public string MimeType { get; set; }
+    }
+}
