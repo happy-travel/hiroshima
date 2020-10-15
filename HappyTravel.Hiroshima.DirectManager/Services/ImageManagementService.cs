d59457a5107a68fda32ca1a4d5bc8fd498b74c6a
Alexey Shokarev <alexey.shokarev@opentech.kz>
2020-10-15 11:32:43 +0600

    Added validations for image

--- HappyTravel.Hiroshima.DirectManager/Services/ImageManagementService.cs
+++ HappyTravel.Hiroshima.DirectManager/Services/ImageManagementService.cs
@@ -4,6 +4,7 @@ using HappyTravel.Hiroshima.Common.Models;
 using HappyTravel.Hiroshima.Data;
 using HappyTravel.Hiroshima.Data.Extensions;
 using HappyTravel.Hiroshima.DirectManager.RequestValidators;
+using Imageflow.Fluent;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
@@ -35,10 +36,55 @@ namespace HappyTravel.Hiroshima.DirectManager.Services
                     return validationResult.IsFailure ? Result.Failure<ContractManager>(validationResult.Error) : Result.Success(contractManager);
                 })
                 .Map(contractManager => Create(contractManager.Id, image))
+                //.Map(dbImage => ValidateImage(image.UploadedFile))
                 .Map(dbImage => AddImage(dbImage, image.UploadedFile))
                 .Ensure(dbImage => dbImage != null, $"Error saving image")
                 .Map(dbImage => Build(dbImage));
 
+            async Task<Result> ValidateImage(FormFile uploadedFile)
+            {
+                // Validation image type
+                var extension = Path.GetExtension(uploadedFile.FileName).ToLower();
+                if ((extension != ".ipg") && (extension != ".ipeg") && (extension != ".png"))
+                    return Result.Failure("Invalid image file type");
+
+                byte[] imageBytes = null;
+                using (BinaryReader binaryReader = new BinaryReader(uploadedFile.OpenReadStream()))
+                {
+                    imageBytes = binaryReader.ReadBytes((int)imageBytes.Length);
+                }
+
+                var info = await ImageJob.GetImageInfo(new BytesSource(imageBytes));
+
+                // Validation image size
+                if ((info.ImageWidth < minimumImageWidth) || (info.ImageHeight < minimumImageHeight))
+                    return Result.Failure("Uploading picture size is less than the minimum");
+
+                // Validation image dimension difference
+                if ((info.ImageWidth / info.ImageHeight > 2) || (info.ImageHeight / info.ImageWidth > 2))
+                    return Result.Failure("Uploading picture dimension difference is more then the maximum");
+
+
+                using (var b = new ImageJob())
+                {
+                    
+                    var r = await b.Decode(imageBytes).
+                        Constrain(new Constraint(ConstraintMode.Fit, 160, 120))
+                        .Branch(f => f.ConstrainWithin(80, 60).EncodeToBytes(new WebPLosslessEncoder()))
+                        .Branch(f => f.ConstrainWithin(40, 30).EncodeToBytes(new WebPLossyEncoder(85)))
+                        .EncodeToBytes(new MozJpegEncoder(targetJpegQuality, true))
+                        .Finish().InProcessAsync();
+
+                    //Assert.Equal(60, r.TryGet(1).Width);
+                    //Assert.Equal(30, r.TryGet(2).Width);
+                    //Assert.Equal(120, r.TryGet(3).Width);
+                    //Assert.True(r.First.TryGetBytes().HasValue);
+                    var i = 0;
+                }
+
+
+                return Result.Success();
+            }
 
             async Task<Image> AddImage(Image dbImage, FormFile uploadedFile)
             {
@@ -46,6 +92,8 @@ namespace HappyTravel.Hiroshima.DirectManager.Services
                 dbImage.Id = Guid.NewGuid();
                 dbImage.Key = $"{S3FolderName}/{dbImage.AccommodationId}/{dbImage.Id}{extension}";
                 dbImage.Created = DateTime.UtcNow;
+
+                await ValidateImage(uploadedFile);
 
                 // Add document to Amazon S3
                 var result = await _amazonS3ClientService.Add(_bucketName, dbImage.Key, uploadedFile.OpenReadStream());
@@ -107,6 +155,11 @@ namespace HappyTravel.Hiroshima.DirectManager.Services
 
 
         private const string S3FolderName = "images";
+        private const long minimumImageWidth = 500;
+        private const long minimumImageHeight = 300;
+        private const long maximumSideSizeLarge = 1600;
+        private const long maximumSideSizeSmall = 400;
+        private const int targetJpegQuality = 85;
 
 
         private readonly IContractManagerContextService _contractManagerContext;
