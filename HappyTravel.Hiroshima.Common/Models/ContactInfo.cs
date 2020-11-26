298f1cef3ac378afbc2d2b619ea91a7b781e1512
Oleg Konovalov <oleg.konovalov@happytravel.com>
2020-11-27 01:02:17 +0300

    Add code review fixes

--- HappyTravel.Hiroshima.Common/Models/ContactInfo.cs
+++ HappyTravel.Hiroshima.Common/Models/ContactInfo.cs
@@ -7,20 +7,20 @@ namespace HappyTravel.Hiroshima.Common.Models
 {
     public class ContactInfo
     {
-        public List<string> Email { get; set; } = new List<string>();
+        public List<string> Emails { get; set; } = new List<string>();
 
-        public List<string> Phone { get; set; } = new List<string>();
+        public List<string> Phones { get; set; } = new List<string>();
 
-        public List<string> Website { get; set; } = new List<string>();
+        public List<string> Websites { get; set; } = new List<string>();
 
         
         public override bool Equals(object? obj) => obj is ContactInfo other && Equals(other);
 
 
         public bool Equals(in ContactInfo other)
-            => Email.SafeSequenceEqual(other.Email) && Phone.SafeSequenceEqual(other.Phone) && Website.SafeSequenceEqual(other.Website);
+            => Emails.SafeSequenceEqual(other.Emails) && Phones.SafeSequenceEqual(other.Phones) && Websites.SafeSequenceEqual(other.Websites);
 
         
-        public override int GetHashCode() =>  Hash.Aggregate(Email, Hash.Aggregate(Phone, Hash.Aggregate(Website, 0)));
+        public override int GetHashCode() =>  Hash.Aggregate(Emails, Hash.Aggregate(Phones, Hash.Aggregate(Websites, 0)));
     }
 }
