1784d6448815c132d1874ca8bca36966ea0544b6
Alexey Shokarev <alexey.shokarev@gmail.com>
2020-12-15 00:50:01 +0600

    Added migration script

---
+++ HappyTravel.Hiroshima.Data/Migrations/20201130170145_ChangedManagersTableAddedPermissions.sql
@@ -0,0 +1,10 @@
+﻿START TRANSACTION;
+
+ALTER TABLE "Managers" ADD "IsMaster" boolean NOT NULL DEFAULT FALSE;
+
+ALTER TABLE "Managers" ADD "Permissions" integer NOT NULL DEFAULT 2147483646;
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20201130170145_ChangedManagersTableAddedPermissions', '5.0.0');
+
+COMMIT;
