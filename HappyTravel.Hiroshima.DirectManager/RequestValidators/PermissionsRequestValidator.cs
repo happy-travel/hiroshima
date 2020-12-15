3e20977e283e1bd3347a2aa3261a00336ab49ea6
Alexey Shokarev <alexey.shokarev@gmail.com>
2020-12-15 17:01:34 +0600

    Added public modifier

--- HappyTravel.Hiroshima.DirectManager/RequestValidators/PermissionsRequestValidator.cs
+++ HappyTravel.Hiroshima.DirectManager/RequestValidators/PermissionsRequestValidator.cs
@@ -3,7 +3,7 @@ using HappyTravel.Hiroshima.DirectManager.Models.Requests;
 
 namespace HappyTravel.Hiroshima.DirectManager.RequestValidators
 {
-    class PermissionsRequestValidator : AbstractValidator<Permissions>
+    public class PermissionsRequestValidator : AbstractValidator<Permissions>
     {
         public PermissionsRequestValidator()
         {
