7fe3e45d45ad59f15d7c53e94636071ae1cfe66c
Oleg Konovalov <oleg.konovalov@happytravel.com>
2020-12-18 17:42:43 +0300

    Fix the webhook endpoint

--- HappyTravel.Hiroshima.DirectManager/Infrastructure/HttpClients/BookingWebhookClient.cs
+++ HappyTravel.Hiroshima.DirectManager/Infrastructure/HttpClients/BookingWebhookClient.cs
@@ -6,6 +6,7 @@ using CSharpFunctionalExtensions;
 using HappyTravel.Hiroshima.DirectManager.Infrastructure.Logging;
 using HappyTravel.Hiroshima.DirectManager.Infrastructure.Options;
 using HappyTravel.Hiroshima.DirectManager.Models.Webhooks.Bookings;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
@@ -13,42 +14,36 @@ namespace HappyTravel.Hiroshima.DirectManager.Infrastructure.HttpClients
 {
     public class BookingWebhookClient
     {
-        public BookingWebhookClient(HttpClient httpClient, IOptions<BookingWebhookOptions> options, ILogger<BookingWebhookClient> logger)
+        public BookingWebhookClient(HttpClient httpClient, IOptions<BookingWebhookOptions> webhookOptions, IOptions<JsonOptions> jsonOptions, ILogger<BookingWebhookClient> logger)
         {
             _httpClient = httpClient;
-            _options = options.Value;
+            _webhookOptions = webhookOptions.Value;
+            _jsonOptions = jsonOptions.Value;
             _logger = logger;
         }
 
 
         public async Task<Result> Send(BookingWebhookData webhookData)
         {
-            var serializedData = JsonSerializer.Serialize(webhookData);
-            var error = $"Failed to execute a request to {_options.WebhookUrl}";
-            try
+            var serializedData = JsonSerializer.Serialize(webhookData, _jsonOptions.JsonSerializerOptions);
+            var error = $"Failed to execute a request to {_webhookOptions.WebhookUrl}";
+
+            var message = new HttpRequestMessage(HttpMethod.Post, _webhookOptions.WebhookUrl)
             {
-                var message = new HttpRequestMessage(HttpMethod.Post, _options.WebhookUrl)
-                {
-                    Content = new StringContent(serializedData)
-                };
-                
-                var response = await _httpClient.SendAsync(message);
-                if (!response.IsSuccessStatusCode)
-                    return Result.Failure($"{error} {nameof(response.StatusCode)} '{response.StatusCode}'");
-            }
-            catch(Exception ex)
-            {
-                _logger.LogBookingWebhookClientException(ex);
-                
-                return Result.Failure(error);
-            }
+                Content = new StringContent(serializedData)
+            };
+
+            var response = await _httpClient.SendAsync(message);
+            if (!response.IsSuccessStatusCode)
+                return Result.Failure($"{error} {nameof(response.StatusCode)} '{response.StatusCode}'");
 
             return Result.Success();
         }
 
 
         private readonly HttpClient _httpClient;
-        private readonly BookingWebhookOptions _options;
+        private readonly BookingWebhookOptions _webhookOptions ;
+        private readonly JsonOptions _jsonOptions;
         private readonly ILogger<BookingWebhookClient> _logger;
     }
 }
