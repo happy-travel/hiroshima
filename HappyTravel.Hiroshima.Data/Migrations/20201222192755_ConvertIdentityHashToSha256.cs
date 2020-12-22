420614eaa6349d60dc669c5578a0a3358a5f4cf1
Oleg Konovalov <oleg.konovalov@happytravel.com>
2020-12-22 22:56:43 +0300

    Convert an Identity hash to Sha256

---
+++ HappyTravel.Hiroshima.Data/Migrations/20201222192755_ConvertIdentityHashToSha256.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HappyTravel.Hiroshima.Data.Migrations
+{
+    public partial class ConvertIdentityHashToSha256 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sql = "UPDATE \"Managers\" SET \"IdentityHash\" = encode(sha256(\"IdentityHash\"::bytea), 'hex')";
+            migrationBuilder.Sql(sql);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        { }
+    }
+}
---
+++ HappyTravel.Hiroshima.DirectContracts/Services/ISha256HashGenerator.cs
@@ -0,0 +1,9 @@
+﻿namespace HappyTravel.Hiroshima.DirectContracts.Services
+{
+    public interface ISha256HashGenerator
+    {
+        string Generate(string source);
+
+        string Generate(byte[] bytes);
+    }
+}
