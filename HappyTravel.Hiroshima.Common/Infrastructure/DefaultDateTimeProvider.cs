8ce7f74f96b27609e17eb2469c58c7f802cce070
Алексей Шокарев <Alexey.Shokarev@gmail.com>
2020-12-30 23:20:28 +0600

    Added IDateTimeProvider and DefaultDateTimeProvider

---
+++ HappyTravel.Hiroshima.Common/Infrastructure/DefaultDateTimeProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HappyTravel.Hiroshima.Common.Infrastructure
+{
+    public class DefaultDateTimeProvider : IDateTimeProvider
+    {
+        public DateTime UtcNow() => DateTime.UtcNow;
+
+        public DateTime UtcTomorrow() => DateTime.UtcNow.AddDays(1).Date;
+
+        public DateTime UtcToday() => DateTime.UtcNow.Date;
+    }
+}
