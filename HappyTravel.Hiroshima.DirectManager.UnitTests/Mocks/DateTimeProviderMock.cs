30b8801b70e6eb6ce8ac398ab0146d97a40f34e5
Алексей Шокарев <Alexey.Shokarev@gmail.com>
2021-01-12 14:17:48 +0600

    Added tests project and DateTimeProviderMock

---
+++ HappyTravel.Hiroshima.DirectManager.UnitTests/Mocks/DateTimeProviderMock.cs
@@ -0,0 +1,23 @@
+﻿//using HappyTravel.Hiroshima.
+using HappyTravel.Hiroshima.Common.Infrastructure;
+using System;
+
+namespace HappyTravel.Hiroshima.DirectManager.UnitTests.Mocks
+{
+    public class DateTimeProviderMock : IDateTimeProvider
+    {
+        public DateTimeProviderMock(DateTime dateTime)
+        {
+            _dateTime = dateTime;
+        }
+
+
+        private readonly DateTime _dateTime;
+
+        public DateTime UtcNow() => _dateTime;
+
+        public DateTime UtcTomorrow() => _dateTime.AddDays(1).Date;
+
+        public DateTime UtcToday() => _dateTime.Date;
+    }
+}
---
+++ HappyTravel.Hiroshima.DirectManager.UnitTests/Tests/Services/AccommodationManagementServiceTests/AccommodationManagementServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HappyTravel.Hiroshima.DirectManager.UnitTests.Tests.Services.AccommodationManagementServiceTests
+{
+    public class AccommodationManagementServiceTests
+    {
+        [Fact]
+        public async Task Agency_mismatch_must_fail_get_agent()
+        {
+            //var (_, isFailure, _, _) = await _agentService.GetAgent(4, AgentContext);
+            var isFailure = false;
+            Assert.True(isFailure);
+        }
+    }
+}
